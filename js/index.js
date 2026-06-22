087cc99e620f44aff0f39d9a4594b0dac645a8f4
rodriguezcausilbreiner-sudo <matatis31@gmail.com>
2026-06-22 09:44:15 -0500

    add

--- js/index.js
+++ js/index.js
@@ -5,3 +5,43 @@
 // Swal.fire() modernos
 
 // wnow
+
+document.querySelector('.btn-ingresar').addEventListener('click', function(event){
+event.preventDefault();
+ 
+const email = document.getElementById('email').value.trim();
+const password = document.getElementById('password').value.trim();
+ 
+ 
+if(email === "" || password ===""){
+ 
+    Swal.fire({
+        icon:"warning",
+        title: "Campos Vacios",
+        text: "Por favor complete los campos",
+        confirmButtonColor:'#800e13'
+    }) ;
+    return;
+ 
+}
+if (email === 'admin@maskot.com' && password == '12345') {
+    Swal.fire({
+        icon: 'success',
+        title: 'Inicio Exitoso',
+        text: 'Bienvenido Administrador',
+        confirmButtonColor: '#800e13'
+    }).then(() =>{
+        window.location.href='Administrador/dashboard.html';
+
+    })
+}else{
+    Swal.fire({
+        icon:"error",
+        title: "Credenciales Incorrectas",
+        text: "Correo o Contraseña Incorrecta",
+        confirmButtonColor: '#800e13'
+    })
+}
+ 
+
+})
