e8f2bb20d0d356ce081e6c0d802366be41f962fc
rodriguezcausilbreiner-sudo <matatis31@gmail.com>
2026-06-24 09:36:50 -0500

    add

---
+++ js/dasboard.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+  $("#tablaUsuarios").DataTable({
+    language: {
+      search: "Buscar",
+      lengthMenu: "Mostrar_Menú_Registro",
+      info: "Mostrar _START_ a _END_ de _TOTAL_ registros",
+      infoEmpty: "Mostrando 0 a 0 de 0 registros",
+      zeroRecords: "No se encontraron resultados",
+      emptyTable: "No hay datos disponibles en la tabla",
+      paginate: {
+        first: "Primero",
+        last: "Último",
+        next: "Siguiente",
+        previous: "Anterior",
+      },
+    },
+  });
+});
+ 
+ 
