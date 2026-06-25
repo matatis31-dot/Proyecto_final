9e5a3d3a4ef5611e3f2c90c5c90bd40a4fac77f3
rodriguezcausilbreiner-sudo <matatis31@gmail.com>
2026-06-25 09:31:46 -0500

    greagar

--- js/reportes.js
+++ js/reportes.js
@@ -21,3 +21,81 @@ new Chart(ctxVentas,{
         }
     }
 })
+
+const ctxServicios = document.getElementById('graficoServicios');
+new Chart(ctxServicios,{
+      type: 'doughnut',
+      data: {
+        labels:['Consulta', 'Vacunacion', 'Baño', 'Peluqueria', 'Urgencias'],
+        datasets:[{
+        label: 'Servicios',
+        data:[35, 25, 15, 5, 45],
+        backgroundColor:[
+        '#72d8ba',
+        '#5e60c4',
+        '#d67bd2',
+        '#d5e094',
+        '#c7a76d',
+        ]
+      }]
+      },
+      options:{
+        responsive: true,
+        cutout: '55',
+        plugins:{
+            legend:{
+                position:'bottom'
+            }
+        }
+      }
+})
+
+const ctxProductos = document.getElementById('graficoProducto');
+new Chart(ctxProductos, {
+type: 'line',
+    data:{
+        labels: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio'],
+        datasets:[{
+            label: 'Clientes Registrados',
+            data:[ 100, 230, 43, 98, 110, 24],
+            backgroundColor: '#4b0c0a'
+        }]
+    },
+ 
+    options:{
+        responsive:true,
+        plugins:{
+            legend:{
+                display:true
+            },
+            title:{
+                display:false
+            }
+        }
+    }
+ 
+});
+const canvasPolar = document.getElementById("graficoPolar");
+new Chart(canvasPolar, {
+  type: "polarArea",
+  data: {
+    labels: ["Perros", "Gatos", "Aves", "Hámsters"],
+    datasets: [
+      {
+        label: "Pacientes atendidos",
+        data: [120, 85, 45, 25],
+        backgroundColor: [
+          "#dd403a",
+          "#2e86ab",
+          "#f6ae2d",
+          "#00dbc2",
+        ],
+      },
+    ],
+  },
+  options: {
+    responsive: true,
+  },
+});
+ 
+
